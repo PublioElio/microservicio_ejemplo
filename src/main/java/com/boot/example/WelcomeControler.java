1b22b59c2bd51874fd2052895c3d5b698afb1cb9
Adriano Díaz Benítez <adrianodiazb@gmail.com>
2025-05-27 23:39:42 +0200

    Custom greeting added

--- src/main/java/com/boot/example/WelcomeControler.java
+++ src/main/java/com/boot/example/WelcomeControler.java
@@ -3,6 +3,7 @@ package com.boot.example;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,10 +15,16 @@ public class WelcomeControler {
 	}
 
 	@GetMapping(value = "welcome/{name}", produces = MediaType.TEXT_PLAIN_VALUE)
-	public String customWelcome(@PathVariable String name) {
+	public String customGreeting(@PathVariable String name) {
 		if (name == null || name.isBlank()) {
 			return "Please provide a valid name!";
 		}
 		return "Welcome to my Spring Boot service, " + name + "!";
+	}
+
+	@GetMapping(value = "customgreeting", produces = MediaType.TEXT_PLAIN_VALUE)
+	public String customGreeting(@RequestParam(required = false, defaultValue = "Guest") String name,
+			@RequestParam(required = false, defaultValue = "42") int age) {
+		return "Welcome, " + name + " you are " + age + " years old.";
 	}
 }
